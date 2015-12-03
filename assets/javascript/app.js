67232ea4f031bc467b9d826600188292d5100c61
Danny Weeks <danny@dannyweeks.com>
2015-12-03 18:54:44 +0000

    external links open in to tab

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,3 +13,12 @@ $(window).load(function () {
     });
 
 });
+
+$.expr[':'].external = function(obj){
+    return !obj.href.match(/^mailto\:/)
+        && (obj.hostname != location.hostname)
+        && !obj.href.match(/^javascript\:/)
+        && !obj.href.match(/^$/)
+};
+
+$('a:external').attr('target', '_blank');
