cc977dbcbe9c114c965d0e73689dabd8fa1f6176
Danny Weeks <danny@dannyweeks.com>
2015-12-03 15:19:03 +0000

    Fix gallery overlap issue

--- source/js/all.js
+++ source/js/all.js
@@ -254,7 +254,7 @@ hljs.initHighlightingOnLoad();
 
 $(document).foundation();
 
-$(window).load(function(){
+$(window).load(function () {
 
     $('#masonry-container').masonry({
 
@@ -265,12 +265,12 @@ $(window).load(function(){
 });
 
 
-(function(){
+$(window).load(function () {
 
     var galleries = {};
     var images = $('img[alt^="gallery-"]');
 
-    $.each(images, function(key, imageElement){
+    $.each(images, function (key, imageElement) {
         var image = $(imageElement);
         var galleryName = image.attr('alt');
         var imageSrc = image.attr('src');
@@ -283,22 +283,32 @@ $(window).load(function(){
 
     });
 
-    $.each(galleries, function(galleryName, gallery){
-        var lastOccurence = $('img[alt="' + galleryName + '"]').last();
-        lastOccurence.after('<div class="row small-up-2 medium-up-3 large-up-4" id="' + galleryName + '"></div>');
+    $.each(galleries, function (galleryName, gallery) {
+        var lastOccurrence = $('img[alt="' + galleryName + '"]').last();
+        lastOccurrence.after('<div class="small-12 columns"><div class="row small-up-2 medium-up-3 large-up-4" id="' + galleryName + '"></div></div>');
 
-        $.each(gallery, function(key, imageSrc){
-            $('#' + galleryName).append('<div class="column"><img width="150" class="thumbnail gallery-thumb" src="' + imageSrc + '"></div>');
+        $.each(gallery, function (key, imageSrc) {
+            var galleryElement = $('#' + galleryName);
+            galleryElement.append('<div class="column"><img width="150" class="thumbnail gallery-thumb" src="' + imageSrc + '"></div>');
+
+            galleryElement.masonry({
+
+                itemSelector: '#' + galleryName + ' .column'
+
+            });
+
+            galleryElement.masonry('reloadItems');
+
         });
     });
 
     images.remove();
 
-})();
+});
 
-$('.gallery-thumb').on('click', function(){
+$('.gallery-thumb').on('click', function () {
     var img = $(this);
-    var src =img.attr('src');
+    var src = img.attr('src');
     $('#gallery-modal > img').attr('src', src);
     $('#gallery-modal').foundation('open');
 
