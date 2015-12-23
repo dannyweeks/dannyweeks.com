2ab8d8d8d626e1650b0ea0ee5c606809c075ace1
Danny Weeks <danny@dannyweeks.com>
2015-12-23 16:13:56 +0000

    publish script

--- publish.sh
+++ publish.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
+#! /bin/bash
+branch="${1:?Provide a branch name}"
 
-# Replace "sculpin generate" with "php sculpin.phar generate" if sculpin.phar
-# was downloaded and placed in this directory instead of sculpin having been
-# installed globally.
+git fetch --all --prune
 
-sculpin generate --env=prod
-if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
-
-# Add --delete right before "output_prod" to have rsync remove files that are
-# deleted locally from the destination too. See README.md for an example.
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
-if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
+if git show-ref --verify --quiet "refs/heads/$branch"; then
+    git checkout master
+    git pull origin master
+    git merge $branch
+    git push origin master
+    git branch -d $branch
+    # git push origin :$branch
+else
+    echo "Branch does not exist"
+    exit 0
+fi
