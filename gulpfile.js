b662423bb082af742f6396f78fd68be48a8d6fc6
Danny Weeks <danny@dannyweeks.com>
2016-03-21 20:46:51 +0000

    optimise site

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,15 @@ var elixir = require('laravel-elixir');
 elixir.config.assetsPath = 'assets';
 elixir.config.publicPath = 'source';
 elixir.config.sourcemaps = false;
+elixir.config.production = true;
 
 elixir(function(mix) {
     mix.sass('app.scss')
         .scripts([
+        './.sculpin/components/jquery/jquery.js',
+        './.sculpin/components/foundation/js/foundation.min.js',
+        './.sculpin/components/masonry/masonry.pkgd.js',
+        './.sculpin/components/highlightjs/highlight.pack.js',
         './node_modules/anchor-js/anchor.js',
         './assets/javascript/gallery.js',
         './assets/javascript/spoilers.js',
